01058ed7145895013a1c51b77860606145584c7c
evfim <evfim1234@gmail.com>
2019-05-03 16:45:51 +0300

    changed bugs

--- Course_parsed.py
+++ Course_parsed.py
@@ -117,7 +117,7 @@ def date_transform(a):
     hours=int(b[6].split(":")[0])
     mins=int(b[6].split(":")[1])
 
-    s = {'enero': 1, 'febrero': 2, 'marzo': 3, 'abríl': 4, 'mayo': 5, 'junio': 6, 'julio': 7, 'agosto': 8,
+    s = {'enero': 1, 'febrero': 2, 'marzo': 3, 'abríl': 4,'abril': 4, 'mayo': 5, 'junio': 6, 'julio': 7, 'agosto': 8,
          'septiembre': 9, 'octubre': 10,
          'noviembre': 11, 'diciembre': 12}
     month=int(s[b[3]])
